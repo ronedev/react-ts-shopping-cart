0d84db37b27ed8e1454af9342c588bf10308d825
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-06-26 02:06:47 -0300

    add: creation and implementation of the useLocalStorage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+export function useLocalStorage<T>(key: string, initialValue: T | (()=> T)){
+    const [value, setValue] = useState<T>(()=>{
+        const jsonValue = localStorage.getItem(key)
+
+        console.log(initialValue)
+
+        if(jsonValue != null) return JSON.parse(jsonValue)
+
+        if(typeof initialValue === 'function'){
+            return (initialValue as ()=>T)()
+        }else{
+            return initialValue
+        }
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(key, JSON.stringify(value))
+    }, [key, initialValue])
+
+    return [value, setValue] as [typeof value, typeof setValue]
+}
